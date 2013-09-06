29f5e214544dbebb096ac6a942baa769a8890259
jerry.wang <jerry.wang@jinjiang.com>
2013-09-06 13:31:50 +0800

    add base project

---
+++ client/StoryContinued/StoryContinued/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryContinued
+//
+//  Created by jerry on 13-9-6.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ client/StoryContinued/StoryContinued/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryContinued
+//
+//  Created by jerry on 13-9-6.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
